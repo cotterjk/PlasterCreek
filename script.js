08daa74b0b38af4fde23084c9d5b5f9992bf01e5
Cotter <cotterjk@gmail.com>
2020-07-04 22:40:52 -0400

    Resizing SVG from API call value

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 var wavespeed = 200;
 var wavecolor = 'red';
+var waveheight_str = '';
+
 // USGS Ajax Query
 $('#query').click(function() { //TODO: Shift to on page load, query streamflow and animate from result
 	$.ajax({
@@ -26,7 +28,10 @@ $('#query').click(function() { //TODO: Shift to on page load, query streamflow a
 //Wave animation //Waves generated with getwaves.io
 var svg_canvas = SVG().addTo('#wave_frame').viewbox(0, 0, 1440, 320)
 
-wavePath = svg_canvas.path('M0,224L48,224C96,224,192,224,288,197.3C384,171,480,117,576,96C672,75,768,85,864,96C960,107,1056,117,1152,138.7C1248,160,1344,192,1392,208L1440,224L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z').attr({ fill: 'none', stroke:'red' })
+svg_canvas.attr("preserveAspectRatio", 'none');
+svg_canvas.attr("width",'100%');
+
+wavePath = svg_canvas.path('M0,224L48,224C96,224,192,224,288,197.3C384,171,480,117,576,96C672,75,768,85,864,96C960,107,1056,117,1152,138.7C1248,160,1344,192,1392,208L1440,224L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z').attr({ fill: 'none', stroke:'red'})
 
 function animateWave(wavespeed, wavecolor) {
 	wavePath.attr({ fill: wavecolor, stroke:'none' })
@@ -34,4 +39,7 @@ function animateWave(wavespeed, wavecolor) {
 	  .plot('M0,256L48,229.3C96,203,192,149,288,112C384,75,480,53,576,53.3C672,53,768,75,864,80C960,85,1056,75,1152,69.3C1248,64,1344,64,1392,64L1440,64L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z')
 	  // .plot('M0,128L48,133.3C96,139,192,149,288,128C384,107,480,53,576,53.3C672,53,768,107,864,149.3C960,192,1056,224,1152,245.3C1248,267,1344,277,1392,282.7L1440,288L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z')
 	  .loop(true, true)
+
+    waveheight_str = wavespeed/2 + "px"
+    svg_canvas.attr("height", waveheight_str);
   }
